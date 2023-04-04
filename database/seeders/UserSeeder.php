0a389ad903095d28ba83fe3f3845f33fea65a79b
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-04 18:33:26 +0200

    fix comment

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(1000)->create()->each(function ($user) {
-            $tags = Tag::factory(rand(0, 5))->make(); //crea da 1 a 5 risultati 
+            $tags = Tag::factory(rand(0, 5))->make(); //associa da 0 a 5 tag per user 
             $user->tag()->saveMany($tags);
         });
     }
